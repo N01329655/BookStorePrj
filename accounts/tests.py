265aa06b430a103c6705db43bad86e9d048219b8
george <georgenechyporenko@gmail.com>
2021-07-13 18:39:44 -0700

    Added environmental variables

--- accounts/tests.py
+++ accounts/tests.py
@@ -53,7 +53,7 @@ class SignupPageTest(TestCase):
         self.assertEqual(get_user_model().objects.all().count(), 1)
         self.assertEqual(get_user_model().objects.all()[0].username, self.username)
         self.assertEqual(get_user_model().objects.all()[0].email, self.email)
-        
+
 
 '''
     def test_singup_view(self):
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+# to work with environmental variables
+env = Env()
+env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,13 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r*v&!t+gj4l2yx#k59!e!s4zd=8__tt%qbp)70o_%c+q!8%9+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
